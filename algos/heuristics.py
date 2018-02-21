f965cc465805035a251734b4633a63a88e9c582c
Santanu Dey <santanud@atlanta.cs.colostate.edu>
2018-02-21 01:09:27 -0700

    cost function - fix case of goal pillar nonexistence- other fixes with x, z, y stuff

--- algos/heuristics.py
+++ algos/heuristics.py
@@ -9,39 +9,45 @@ def find(f, currentStateList):
 def euclidean(source, dest):
     return np.sqrt((source[0]-dest[0])**2 + (source[1]-dest[1])**2 + (source[2]-dest[2])**2)
 
+
 def costHeuristicFunc(goal, currentStateMap, maxY=51):
-    goalPillar = (goal[0], goal[2])
-    goalPillarBlocksList = currentStateMap[goalPillar]
-    if len(goalPillarBlocksList)-1 >= goal[1] and goalPillarBlocksList[goal[1]] == goal[3]:
-        # already goal achieved, return zero cost
-        return 0 
+    goalPillar = (goal[0], goal[1])
+    if goalPillar not in currentStateMap:
+        goalPillarBlocksList=[]
+    else:
+        goalPillarBlocksList = currentStateMap[goalPillar]
+        
+    goalPillarTopY = len(goalPillarBlocksList)
     
+    if len(goalPillarBlocksList)-1 >= goal[2] and goalPillarBlocksList[goal[2]] == goal[3]:
+        # already goal achieved, return zero cost
+        return 0
+
     # find current drone pos
     # dronePos = find(lambda item: item[3] == 'd', currentStateList)
-    
+
     # to be removed once we have refactored the algos to extract dronePos from current simulator.dronePos
-    dronePos = (-1,-1,-1)
     for pillar, pillarBlocksList in currentStateMap.items():
         if 'd' in pillarBlocksList:
             dronePos = (pillar[0], pillar[1], pillarBlocksList.index('d'))
             break
-    
+
     # euclidean dist from drone pos to goal x, goal z, max(goal pillar y) +1 = len(goalPillarList)
+
     
-    goalPillarTopY = len(goalPillarBlocksList)
-    droneDistToGoalPillarTop = euclidean(dronePos, (goal[0], goal[2], goalPillarTopY))
-    
+    droneDistToGoalPillarTop = euclidean(dronePos, (goal[0], goal[1], goalPillarTopY))
+
     #print('droneDistToGoalPillarTop=', droneDistToGoalPillarTop)
-    #based on goal x, goal y, goal z and current stuff on goal x, goal z - the current pillar at goal x, goal z, 
+    #based on goal x, goal y, goal z and current stuff on goal x, goal z - the current pillar at goal x, goal z,
     #determine how many blocks on goal x, goal z pillar needs move out ? - let's call it cntMoveOuts
-    
-    cntMoveOuts = (goalPillarTopY - 1 - goal[1]) + 1
-    
-    # For each moveout, examine each of the other (101*101)-1 pillars on table (minus to exclude goal pillar) and try to 
+
+    cntMoveOuts = (goalPillarTopY - 1 - goal[2]) + 1
+
+    # For each moveout, examine each of the other (101*101)-1 pillars on table (minus to exclude goal pillar) and try to
     # fit as many blocks out of cntMoveOuts from 3.1 above on to these destination pillars. Cost for these moveouts is:
     #   moveoutscost= Sum of all such
 
-    #      (attach=1 + 
+    #      (attach=1 +
     #      2*move=2*(euclidean from goal x, goal z, tobemovedout y on goal pillar to moveout destination x,y,z) +
     #      release=1)
     #      move cost is doubled for drone to and fro trips.
@@ -49,29 +55,30 @@ def costHeuristicFunc(goal, currentStateMap, maxY=51):
     #print('cntMoveOuts=', cntMoveOuts)
     moveOutCost=0
     toBeMovedOut=cntMoveOuts
+
     for pillar, pillarBlocksList in currentStateMap.items():
         if toBeMovedOut == 0:
             break
         if (pillar != goalPillar):
             pillarTopY = len(pillarBlocksList)
             destsOnPillar = maxY - pillarTopY
-            
+
             for i in range(destsOnPillar):
-                distToDestOnPillar = euclidean((goal[0], goalPillarTopY-1-i, goal[2]), (pillar[0], pillarTopY-1-i, pillar[1]))
+                distToDestOnPillar = euclidean((goal[0], goal[1], goalPillarTopY-1-i), (pillar[0], pillar[1], pillarTopY-1-i, ))
                 moveOutCost = moveOutCost + 2*distToDestOnPillar + 1 + 1
                 toBeMovedOut = toBeMovedOut-1
                 if toBeMovedOut == 0:
                     break
     # How many blocks we need to move in to goal pillar? let's call it cntMoveIns
     #print('moveOutCost=', moveOutCost)
-    
-    cntMoveIns = (goal[1] + 1) - (goalPillarTopY-cntMoveOuts)
-    #print('cntMoveIns=', cntMoveIns)  
-    #For each move in, examine each of the other (101*101)-1 pillars on table (minus to exclude goal pillar) and try to 
+
+    cntMoveIns = (goal[2] + 1) - (goalPillarTopY-cntMoveOuts)
+    #print('cntMoveIns=', cntMoveIns)
+    #For each move in, examine each of the other (101*101)-1 pillars on table (minus to exclude goal pillar) and try to
     # accumulate as many blocks out of cntMoveIns from 3.3 above from these source pillars. Cost for these moveins is:
     #     moveinscost= Sum of all such
 
-    #     (attach=1 + 
+    #     (attach=1 +
     #     2*move=2*(euclidean from source x, y, z to goal x, goal z, tobemovedin y on goal pillar) +
     #     release=1)
     #     move cost is doubled for drone to and fro trips.
@@ -81,16 +88,16 @@ def costHeuristicFunc(goal, currentStateMap, maxY=51):
         if cntMoveIns == 0:
             break
         if (pillar != goalPillar):
-            sourcesOnPillar = len(pillarBlocksList) 
+            sourcesOnPillar = len(pillarBlocksList)
             pillarTopY = len(pillarBlocksList)
-            
+
             for i in range(sourcesOnPillar):
-                distFromSrcOnPillar = euclidean((goal[0], goalPillarTopY-1+i, goal[2]), (pillar[0], pillarTopY-1-i, pillar[1]))
+                distFromSrcOnPillar = euclidean((goal[0], goal[1], goalPillarTopY-1+i), (pillar[0], pillar[1], pillarTopY-1-i))
                 moveInCost = moveInCost + 2*distFromSrcOnPillar + 1 + 1
                 cntMoveIns = cntMoveIns-1
                 if cntMoveIns == 0:
                     break
-    #print('moveInCost=', moveInCost)                
+    #print('moveInCost=', moveInCost)
     return droneDistToGoalPillarTop + moveOutCost + moveInCost
 
 # test
@@ -117,11 +124,10 @@ for stateElement in currentStateList:
 for pillarcoordinate, pillar in currentStateMap.items():
     print(pillarcoordinate)
     print(pillar)
-
-dronePos =(0, -1, 1)
-goal = (0,1,0,'green')
+    
+goal = (0,0,1,'green')
 
 print('cost to achieve {} is {}'.format(goal, costHeuristicFunc(goal, currentStateMap, maxY=2)))
 
-goal = (0,1,0,'yellow')
+goal = (0,0,1,'yellow')
 print('cost to achieve {} is {}'.format(goal, costHeuristicFunc(goal, currentStateMap, maxY=2)))
