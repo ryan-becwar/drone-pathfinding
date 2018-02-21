af40e93aedcd072e728cd5765c496333896ffdd7
Ryan Becwar <rbecwar@cs.colostate.edu>
2018-02-20 22:24:51 -0700

    Adding more state functions

--- runsim.py
+++ runsim.py
@@ -1,14 +1,27 @@
 from simulator import *
+from algos.heuristics import costHeuristicFunc
 import algos.recursiveBestFirstAstar as a
 
 
+def testF(possible_commandsF):
+    print(possible_commandsF())
 
 sim = Simulator.from_file("gamestates/simple")
-print(sim.to_list())
-s2 = Simulator(sim.state())
-print(s2.to_list())
-#locations = sim.to_list()
-#dimensions = (s.WIDTH, s.LENGTH, s.HEIGHT)
+
+actions = sim.possible_commands()
+print(actions)
+print()
+after = resultingStateFromAction(sim.state(), actions[0])
+print(after)
+
+print()
+print(testF(sim.possible_commands))
 
 
-#a.aStarSearch(s.state(), s.possibleActionsF, resultingStateFromActionF, goalTestF, heuristicF)
+result = a.aStarSearch(sim.state(), possibleActions, resultingStateFromAction, goalTest, costHeuristicFunc)
+
+print()
+print(result)
+
+
+
