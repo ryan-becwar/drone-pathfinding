352345f96b289792a7422d7ebe2dc37d33e6e858
Santanu Dey <santanud@atlanta.cs.colostate.edu>
2018-03-06 23:39:54 -0700

    Some last minute changes to SA and runsimSA.py

--- algos/simulatedAnnealing.py
+++ algos/simulatedAnnealing.py
@@ -14,6 +14,67 @@ def simulatedAnneal(startState, heuristicF, goal, TdecayFactor=0.90):
     T = 1.00000
 
     nodesExplored = 0
+    numCycles=0
+    while True :
+        result.append(currentState)
+        
+        if currentState.goalTest(goal):
+            return (result, nodesExplored)
+        possibleActions = currentState.possibleActions()
+
+        #print(possibleActions)
+
+        action = pickRandomAction(possibleActions)
+
+        #print('Action chosen ', action, 'dronePos ', currentState.drone_pos)
+
+        actionsTaken.append(action)
+
+        nextState = currentState.resultingStateFromAction(action)[0]
+
+        #print('New dronePos', nextState.drone_pos)
+
+        if nextState.goalTest(goal):
+            result.append(nextState)
+            return (result, actionsTaken, nodesExplored)
+
+        nextStateCost = heuristicF(goal, nextState.currentStateMap, nextState.drone_pos)
+   
+        dE = nextStateCost - currentStateCost
+
+        #print('dE={}, nextCost={}, currentCost={}'.format(dE, nextStateCost, currentStateCost)) 
+
+        if T <=0:
+            print('T is <=0')
+            p=1.0
+        else:
+            qt = (abs(dE))/T 
+            p = np.exp(-qt)
+        
+        #print('T=', T, 'p=',p)
+
+        if dE <= 0 or p > random.random():
+            #print('Chosing next state')                       
+            nodesExplored = nodesExplored + 1
+            currentState = nextState
+            currentStateCost = nextStateCost
+
+        T = T*TdecayFactor
+
+        numCycles = numCycles + 1
+
+def simulatedAnnealV1(startState, heuristicF, goal, TdecayFactor=0.90):
+    result =[]
+    actionsTaken=[]
+    currentState = startState
+    currentStateCost = heuristicF(goal, currentState.currentStateMap, currentState.drone_pos)
+    T = 1.00000
+    dEAvg = 0.00000
+    numAccepted=0.0
+    numCycles = 0.0
+
+    nodesExplored = 0
+
     while True:
         result.append(currentState)
         if currentState.goalTest(goal):
@@ -29,21 +90,39 @@ def simulatedAnneal(startState, heuristicF, goal, TdecayFactor=0.90):
             return (result, actionsTaken, nodesExplored)
 
         nextStateCost = heuristicF(goal, nextState.currentStateMap, nextState.drone_pos)
-        dE = round(nextStateCost - currentStateCost, 5)
+        dE = nextStateCost - currentStateCost
 
+        dEAbs = round(abs(dE), 5)
 
-        if T <=0:
-            print('T is <=0')
-            probabilityOfAcceptingRandomAction=np.longdouble(1.0)
+        if not T > 0.00000:
+           print('T zero')
+
+        if nextStateCost > currentStateCost:
+            # worse
+            if numCycles == 0:
+               dEAvg = dEAbs
+
+            qt = round((dEAbs)/(dEAvg*T), 5)
+            p = np.longdouble(np.exp(-qt))
+            if p > np.longdouble(random.random()):
+               # but p greater than random p, so accept anyway
+               print('worse but high p, accepting')
+               accept = True
+            else:
+               print('worse and low p, not accepting')
+               accept = False 
         else:
-            qt = round((abs(dE))/T, 5)
-            #qt = round(dE/T, 5)
-            probabilityOfAcceptingRandomAction = np.longdouble(np.exp(-qt))
+            # better anyway so accept 
+            print('better so accepting')
+            accept = True
+        
+        if accept :
+           nodesExplored = nodesExplored + 1 
+           currentState = nextState
+           currentStateCost = nextStateCost
+           numAccepted = numAccepted + 1.0
+           dEAvg = round((dEAbs * (numAccepted-1.0) +  dEAbs) / numAccepted, 5)
 
-        if dE <= 0 or probabilityOfAcceptingRandomAction > np.longdouble(random.random()):
-            nodesExplored = nodesExplored + 1
-            currentState = nextState
-            currentStatecost = nextStateCost
 
         T = round(T*TdecayFactor,5)
 
@@ -88,7 +167,7 @@ def simulatedMoreAnnealAtSameT(startState, heuristicF, goal, TdecayFactor=0.8, n
             if dE <= 0 or probabilityOfAcceptingRandomAction > np.longdouble(random.random()):
                 nodesExplored = nodesExplored + 1
                 currentState = nextState
-                currentStatecost = nextStateCost
+                currentStateCost = nextStateCost
 
         T = round(T*TdecayFactor, 5)
     
--- runsimSA.py
+++ runsimSA.py
@@ -1,4 +1,4 @@
-from simulator import *
+from newsimulator import *
 from algos.heuristics import costHeuristicFunc
 import algos.simulatedAnnealing as sa
 import time as time
@@ -6,35 +6,34 @@ import time as time
 
 def testF(possible_commandsF):
     print(possible_commandsF())
-
+numruns=1
 numactions=0
 times=0
-for i in range(20):
-    sim = Simulator.from_file("gamestates/simple")
+for i in range(numruns):
+    sim = NewSimulator.from_file("gamestates/simple")
 
     goal = (3,3,0,'red')
     before=time.time()
     result = sa.simulatedAnneal(sim, costHeuristicFunc, goal)
     after=time.time()
-    print('[{}]simple SA {} actions taken in {} seconds'.format(i,len(result[1]), after-before))
+    print('[{}]  simple SA {} actions taken in {} seconds'.format(i,len(result[1]), after-before))
 
     numactions = numactions + len(result[1])
     times = times + after-before
-print('Average {} actions taken in average {} seconds with simple SA'.format(numactions/20, times/20))
+print('Average {} actions taken in average {} seconds with simple SA'.format(numactions/numruns, times/numruns))
 
 numactionsMore=0
 timesMore=0
-for i in range(20):
-    sim = Simulator.from_file("gamestates/simple")
+for i in range(0):
+    sim = NewSimulator.from_file("gamestates/simple")
 
     goal = (3,3,0,'red')
     before=time.time()
     result = sa.simulatedMoreAnnealAtSameT(sim, costHeuristicFunc, goal)
     after=time.time()
-    print('[{}]SA with more anneal at same T {} actions taken in {} seconds'.format(i,len(result[1]), after-before))
+    print('[{}]  SA with more anneal at same T {} actions taken in {} seconds'.format(i,len(result[1]), after-before))
 
     numactionsMore = numactionsMore + len(result[1])
     timesMore = timesMore + after-before
-print('Average {} actions taken in average {} seconds for SA with more anneal at same T'.format(numactionsMore/20, timesMore/20))
-
+print('Average {} actions taken in average {} seconds for SA with more anneal at same T'.format(numactionsMore/numruns, timesMore/numruns))
 
