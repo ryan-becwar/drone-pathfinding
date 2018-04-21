115be893098eb790f6f1e469de2d5c668e50e8db
Ryan Becwar <rbecwar@cs.colostate.edu>
2018-04-21 17:44:57 -0600

    Fixed major bug in runteir that was increasing run times

--- runtier.py
+++ runtier.py
@@ -15,16 +15,18 @@ print(sim.state())
 print()
 
 #case 1
-goal = (3,3,0,'red')
+#goal = (3,3,0,'red')
 
 #case 2
 #goal = (3,3,2,'red')
 
 #case 3
-#goal = (5,3,4,'red')
+goal = (3,3,3,'red')
 
 start_time = time.time()
 result = a.blockaStar(sim,goal, stack_heuristic)
+
+block_time = time.time()
 
 dronePath = []
 for i in range(0,len(result)-1):
@@ -33,14 +35,14 @@ for i in range(0,len(result)-1):
     xd0, zd0, yd0, _ = action[0]
     xd1, zd1, yd1 = action[1]
 
-    droneResult1 = a.aStar(sim,(xd0, zd0, yd0,'d'), chebyshev_sim)
-    droneResult2 = a.aStar(sim,(xd1, zd1, yd1,'d'), chebyshev_sim)
+    droneResult1 = a.aStar(sim,(xd0, zd0, yd0+1,'d'), euclidean_sim)
+    droneResult2 = a.aStar(sim,(xd1, zd1, yd1+1,'d'), euclidean_sim)
     dronePath.extend(droneResult1)
     dronePath.extend(droneResult2)
 
 
 print()
-print("Time elapsed", time.time() - start_time)
+print("Time elapsed", time.time() - start_time, "block time: ", block_time - start_time)
 print("Path length: ", len(dronePath) + 2 * (len(result)-1))
 print()
 print("RESULT: ")
