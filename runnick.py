a7a95bc5821478c1ed8138a68f29f0612d425c77
Nick Lawrence <conqueralltime@yahoo.com>
2018-04-23 23:15:02 -0600

    --

--- runnick.py
+++ runnick.py
@@ -31,27 +31,12 @@ goals2 = [(0,0,1,'black'),(0,0,0,'red')]
 
 goals3 = [('?', '?', 0, '?'), ('?', '?', 1, '?'), ('?', '?', 2, '?'), ('?', '?', 3, '?'), ('?', '?', 4, '?'), ('?', '?', 5, '?'), ('?', '?', 6, '?'), ('?', '?', 7, '?'), ('?', '?', 8, '?'), ('?', '?', 9, '?'), ('?', '?', 10, '?'), ('?', '?', 11, '?'), ('?', '?', 12, '?'), ('?', '?', 13, '?'), ('?', '?', 14, '?'), ('?', '?', 15, '?'), ('?', '?', 16, '?'), ]
 
+start_time = time.time()
 
 #block_goals = assign.assignGoals(sim, goals1)
 #block_goals = assign.assignGoals(sim, goals2)
 block_goals = assign.assignGoals(sim, goals3)
 res = where_does_my_block_want_to_move(sim, block_goals)
-
-start_time = time.time()
-result = a.blockaStar(sim,goal, stack_heuristic)
-
-dronePath = []
-for i in range(0,len(result)-1):
-    sim, action = result[i]
-    print(result[i])
-    xd0, zd0, yd0, _ = action[0]
-    xd1, zd1, yd1 = action[1]
-
-    droneResult1 = a.aStar(sim,(xd0, zd0, yd0,'d'), chebyshev_sim)
-    droneResult2 = a.aStar(sim,(xd1, zd1, yd1,'d'), chebyshev_sim)
-    dronePath.extend(droneResult1)
-    dronePath.extend(droneResult2)
-
 
 print()
 print("Time elapsed", time.time() - start_time)
